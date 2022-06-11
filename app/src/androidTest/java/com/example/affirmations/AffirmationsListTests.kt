502a7c270811d5da46075180544dea60e73cad1f
Edwin Jarvis <edwin.jarvis@taiwolskit.com>
2022-06-11 17:59:33 +0800

    Fix UI test
    Signed-off-by: Edwin Jarvis <edwin.jarvis@taiwolskit.com>

--- app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.affirmations
 
 import androidx.recyclerview.widget.RecyclerView
@@ -12,7 +27,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class AffirmationListTest {
+class AffirmationsListTests {
+
     @get:Rule
     val activity = ActivityScenarioRule(MainActivity::class.java)
 
@@ -20,14 +36,10 @@ class AffirmationListTest {
     fun scroll_to_item() {
         onView(withId(R.id.recycler_view)).perform(
             RecyclerViewActions
-                .scrollTo<RecyclerView.ViewHolder>(
-                    withText(R.string.affirmation10)
-                )
+                .scrollToPosition<RecyclerView.ViewHolder>(9)
         )
 
         onView(withText(R.string.affirmation10))
-            .check(
-                matches(isDisplayed())
-            )
+            .check(matches(isDisplayed()))
     }
 }
